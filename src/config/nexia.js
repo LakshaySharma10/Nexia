cab521a323c6357212abcfb4037b5e2e5e540c3d
Lakshay Sharma <lakshay7004@gmail.com>
2024-05-23 21:18:11 +0530

    Update nexia.js

--- src/config/nexia.js
+++ src/config/nexia.js
@@ -1,66 +1,10 @@
-// .env
-// GEMINI_API_KEY=AIzaSyAfccBfXgj4AUwMOv9W-q5o1hWN6Z8d2sY;
-
-// import{
-//     GoogleGenerativeAI,
-//     HarmCategory,
-//     HarmBlockThreshold,
-//   }  from "@google/generative-ai";
-  
-//   const apiKey = process.env.GEMINI_API_KEY;
-//   const genAI = new GoogleGenerativeAI(apiKey);
-  
-//   const model = genAI.getGenerativeModel({
-//     model: "gemini-1.5-pro-latest",
-//   });
-  
-//   const generationConfig = {
-//     temperature: 1,
-//     topP: 0.95,
-//     topK: 64,
-//     maxOutputTokens: 8192,
-//     responseMimeType: "text/plain",
-//   };
-  
-//   const safetySettings = [
-//     {
-//       category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-//       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//     },
-//     {
-//       category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-//       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//     },
-//     {
-//       category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-//       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//     },
-//     {
-//       category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-//       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//     },
-//   ];
-  
-//   async function run(prompt) {
-//     const chatSession = model.startChat({
-//       generationConfig,
-//       safetySettings,
-//       history: [
-//       ],
-//     });
-  
-//     const result = await chatSession.sendMessage(prompt);
-//     console.log(result.response.text());
-//   }
-
-// nexia.js
 import {
   GoogleGenerativeAI,
   HarmCategory,
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = 'AIzaSyAfccBfXgj4AUwMOv9W-q5o1hWN6Z8d2sY';
+const apiKey = 'YOUR_API_KEY';
 
 const genAI = new GoogleGenerativeAI(apiKey);
 
